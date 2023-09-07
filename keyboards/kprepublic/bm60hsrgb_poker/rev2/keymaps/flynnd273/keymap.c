1ff6d03f21379892eedade7655cd64a2ca276f77
Flynn Duniho <flynn@harveyosity.org>
2023-09-07 19:38:12 -0400

    remove fn-p printscreen shortcut

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/flynnd273/keymap.c
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/flynnd273/keymap.c
@@ -187,7 +187,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [L_FNL] = LAYOUT_60_ansi(
         KC_GRV,   KC_F1,    KC_F2,      KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,      KC_DEL,
-        KC_LGUI,    XXXXXXX,    XXXXXXX,  XXXXXXX,  G(KC_R),  XX_TERM,  XXXXXXX,  KC_HOME,  XXXXXXX,  KC_END,   KC_PSCR,   XXXXXXX,   XXXXXXX,  XXXXXXX,
+        KC_LGUI,    XXXXXXX,    XXXXXXX,  XXXXXXX,  G(KC_R),  XX_TERM,  XXXXXXX,  KC_HOME,  XXXXXXX,  KC_END,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,
         TO(L_SET),    KC_MPRV,  KC_MPLY,    KC_MNXT,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, XXXXXXX,   XXXXXXX,            _______,
         KC_LSFT,        KC_VOLD,  KC_VOLU,    KC_MUTE,  G(KC_V),  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                    KC_RSFT,
         KC_LCTL, XXXXXXX,  KC_LALT,                               G(KC_UP),                               KC_RALT,   KC_LGUI,          TO(L_DEF),  KC_RCTL
